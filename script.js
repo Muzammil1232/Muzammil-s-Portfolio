c0c0efa6fa9c7c3b0d7d89644d4ed26fc85c6dff
Muzammil1232 <muzammil844803@gmail.com>
2025-11-10 08:58:39 -0800

    hero-update

--- script.js
+++ script.js
@@ -143,6 +143,27 @@ window.onclick = function(event) {
 }
 
 
+const modal = document.getElementById("resumeModal");
+const openBtn = document.getElementById("openResumeBtn");
+const closeBtn = document.getElementById("closeModal");
+
+openBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Close modal if clicked outside content
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+
+
 
 
 
